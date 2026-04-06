135314aed723d749de7c2b2c614d11b8fac992bb
Jihyun Nam <initialfx@gmail.com>
2026-04-06 09:46:48 -0700

    Add say_hello function to core

--- src/openpipe/core/__init__.py
+++ src/openpipe/core/__init__.py
@@ -4,3 +4,7 @@ from .prod import Project, get_shot_info, test_production_logic
 def get_project_name():
     """기본 프로젝트 이름 반환"""
     return "OpenPipe"
+
+def say_hello():
+    """간단한 인사말 반환"""
+    return "Hello from OpenPipe core!"
